eb707b93a6eeeb5673ffebaea2ff66666daa6fbb
raokotirao-cell <raokotirao@gmail.com>
2026-07-30 16:21:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -148,17 +148,10 @@ async function loadFFmpeg() {
 
     // IMPORTANT:
     // Local FFmpeg package
-    const module = await import("/ffmpeg/index.js");
-
-const FFmpeg =
-  module.FFmpeg ||
-  module.default?.FFmpeg ||
-  module.default;
-
-if (typeof FFmpeg !== "function") {
-  throw new Error("FFmpeg class was not found in /ffmpeg/index.js");
-}
-     
+    
+     const { FFmpeg } = await import(
+  "/ffmpeg/classes.js"
+);
     ffmpeg = new FFmpeg();
 
     ffmpeg.on("log", ({ message }) => {
