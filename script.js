ee7a16be2bc6366cae55198545d278a9493d0af8
raokotirao-cell <raokotirao@gmail.com>
2026-07-30 10:54:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
- const button = document.querySelector("button");
+const videoInput = document.getElementById("videoInput");
+const videoPreview = document.getElementById("videoPreview");
+const addVideo = document.getElementById("addVideo");
 
-button.addEventListener("click", () => {
-  alert("Video editor starting...");
+addVideo.addEventListener("click", () => {
+  videoInput.click();
+});
+
+videoInput.addEventListener("change", () => {
+  const file = videoInput.files[0];
+
+  if (!file) return;
+
+  const videoURL = URL.createObjectURL(file);
+
+  videoPreview.src = videoURL;
+  videoPreview.style.display = "block";
 });
