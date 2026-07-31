cb18c93b07e05f48aeca19fb3b82f22b400231c4
raokotirao-cell <raokotirao@gmail.com>
2026-07-31 12:55:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3000,7 +3000,10 @@ videoGain.gain.value = 0;
 
 videoSource.connect(videoGain);
 videoGain.connect(destination);
+    }
+  );
 
+})();
 
         // --------------------------------
         // STREAM
