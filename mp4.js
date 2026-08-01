041029b88ff7581f641ee51a97a42c994614e32e
raokotirao-cell <raokotirao@gmail.com>
2026-08-01 12:39:24 +0530

    Update alert messages and load FFMPEG core

--- mp4.js
+++ mp4.js
@@ -7,20 +7,30 @@ document.addEventListener("DOMContentLoaded", () => {
     alert("STEP 1");
 
     try {
+
       const module = await import(
         "https://cdn.jsdelivr.net/npm/@ffmpeg/ffmpeg@0.12.10/dist/esm/index.js"
       );
 
-      alert("STEP 2 - FFMPEG MODULE LOADED");
+      alert("STEP 2 - MODULE LOADED");
 
       const ffmpeg = new module.FFmpeg();
 
       alert("STEP 3 - CONSTRUCTOR OK");
 
+      alert("STEP 4 - LOADING FFMPEG...");
+
+      await ffmpeg.load({
+        coreURL:
+          "https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.12.10/dist/umd/ffmpeg-core.js"
+      });
+
+      alert("STEP 5 - FFMPEG LOADED ✅");
+
     } catch (error) {
 
       alert(
-        "ERROR:\n\n" +
+        "FFMPEG LOAD ERROR:\n\n" +
         error.message +
         "\n\n" +
         error.stack
